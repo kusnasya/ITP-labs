8c47dee513db9f000f287ff1f1f8367f03840959
Bezgacheva Anastasia <kusnasya@yandex.ru>
2024-01-15 14:33:40 +0300

    labs

--- Lab-1/src/Palindrome.java
+++ Lab-1/src/Palindrome.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Palindrome {
+public class Palindrome {
+    public class Palindrome {
+        public static void main(String[] args) {
+            for (int i = 0; i < args.length; i++) {
+                String s = args[i];
+                System.out.println((isPalindrome(s)));
+            }
+
+        }
+        public static String reverseString(String s){
+            String reverse = "";
+            for (int i = s.length()-1; i >= 0 ; i--){
+                reverse += s.charAt(i);
+            }
+            return reverse.toLowerCase();
+        }
+        public static boolean isPalindrome(String s){
+            if (s.toLowerCase().equals(reverseString(s))){
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
 }
--- Lab-1/src/Primes.java
+++ Lab-1/src/Primes.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Primes {
+public class Primes {
+    public static void main(String[] args){
+        IsPrime(100);
+    }
+    public static boolean IsPrime(int n){
+        for (int i = 2; i < n; i++){
+            boolean flag = true;
+            for (int j = 2; j < (i / 2); j ++){
+                if (i % j == 0){
+                    flag = false;
+                }
+            }
+            if (flag){
+                System.out.println(i);
+            }
+        }
+        return false;
+    }
 }
